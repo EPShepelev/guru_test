9104923ea25dbae3081dd13da0c5cb1f2c1e2f7f
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-31 07:45:49 +0300

    create open tab function

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 fetch("../data/numbers.json")
   .then((res) => res.json())
   .then((data) => console.log(data));
+
+function openTab(e, contentId) {
+  const tabcontent = document.querySelectorAll(".tab-content");
+  tabcontent.forEach((element) => {
+    element.style.display = "none";
+  });
+
+  const tablinks = document.querySelectorAll(".tab-title");
+  tablinks.forEach((element) => {
+    element.classList.remove("active");
+  });
+  document.getElementById(contentId).style.display = "block";
+  e.currentTarget.classList.add("active");
+}
