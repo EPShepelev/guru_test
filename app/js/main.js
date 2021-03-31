af13db256e42332305cf4f420dab447ecaccee05
Shepelev Evgeniy <shepelev.ep@yandex.ru>
2021-03-31 21:06:44 +0300

    add event listener

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,57 @@
 const tableTabs = document.querySelector(".table-tabs");
-const tableContainer = document.querySelector(".table-container");
+const tableContent = document.querySelector(".table-content");
 
 fetch("../data/numbers.json")
   .then((res) => res.json())
   .then((data) => {
     data.numbers.forEach((elem) => {
       if (elem.is_visible) {
-        const header = document.createElement("div");
+        const headerTab = document.createElement("div");
         const tabel = document.createElement("tabel");
 
-        header.innerHTML = `${elem.alias}`;
-        tableTabs.appendChild(header);
+        headerTab.innerHTML = `${elem.alias}`;
+        headerTab.classList.add("tab");
 
-        tableContainer.appendChild(tabel);
-        tabel.classList.add("tabel-content");
+        tableTabs.appendChild(headerTab);
+
+        tableContent.appendChild(tabel);
+        tabel.classList.add("tabel");
+        // tabel.id = `${elem.alias}`;
+
+        // headerTab.onclick = function () {
+        //   document.querySelector(`#${elem.alias}`).classList.toggle("hide");
+        // };
 
         for (key in elem.number_list) {
-          tabel.insertAdjacentHTML(
-            "beforeend",
-            `<tr>
-          <td>${elem.number_list[key].number}</td>
-          <td>${elem.number_list[key].cdate}</td>
-        </tr>`
-          );
+          let row = document.createElement("tr");
+          let numCell = document.createElement("td");
+          let dataCell = document.createElement("td");
+
+          tabel.appendChild(row);
+          row.appendChild(numCell);
+          row.appendChild(dataCell);
+          numCell.innerHTML = `${elem.number_list[key].number}`;
+          dataCell.innerHTML = `${elem.number_list[key].cdate}`;
         }
       }
     });
   });
+
+window.onload = function () {
+  const tabs = document.getElementsByClassName("tab");
+  const tabel = document.getElementsByClassName("tabel");
+
+  [...tabs].forEach((tab) => tab.addEventListener("click", tabClick));
+
+  function tabClick(event) {
+    const tabId = event.target.dataset.id;
+
+    [...tabs].forEach((tab, i) => {
+      tab.classList.remove("active");
+      tabel[i].classList.remove("active");
+    });
+
+    tabs[tabId - 1].classList.add("active");
+    sections[tabId - 1].classList.add("active");
+  }
+};
